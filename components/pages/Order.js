48ab0b36537a90b28d70b3c4adae1042b4dd0813
Madcattk <poshnunp@gmail.com>
2023-07-16 16:42:53 +0700

    update mem order

--- components/pages/Order.js
+++ components/pages/Order.js
@@ -111,7 +111,7 @@ export const Order = ({ form, onChange, onLoad }) => {
                             <div className='w-full grid grid-cols-1 md:grid-cols-3'>
                                 <div className='md:col-span-2 flex gap-2'>
                                     <div>
-                                        <Image src="/assets/images/products/JeanVest.jpg" alt="Product" width={80} height={100}/>
+                                        <Image src={form?.Product?.Product_Image || "/assets/images/avatars/no-image.png"} alt="Product" width={80} height={100}/>
                                     </div>
                                     <div className='flex flex-col font-light'>
                                         <span>{item?.Product_Name || ''}</span>
@@ -154,17 +154,17 @@ export const Review = ({ form, onChange, onLoad }) => {
         <div className='w-full flex_center'>
             <div className='xl:w-[1120px] lg:w-[820px] md:w-[620px] sm:w-96 w-72 flex_center flex-col gap-3 px-5 py-3 border border-brown mb-5'>
                 <div className='w-full flex flex-col md:flex-row md:items-end justify-between font-light'>
-                    <span>ORDER NO: 12342222</span>
-                    <span className='text-sm'>Date: 2023-02-03</span>
-                    <span>Status</span>
+                    <span>ORDER NO: {form?.Sale_Id || ''}</span>
+                    <span className='text-sm'>Date: {DateFormat(form?.Sale_Date) || ''}</span>
+                    <span>{form?.Sale_Status || ''}</span>
                 </div>
                 <div className='w-full py-1 flex flex-col md:flex-row items-start justify-between font-light text-sm border-b border-b-gray border-t border-t-gray'>
                     <div>
-                    <div>Luna Braise</div>
-                    <div>114/1 Siphya Road Mahapruetharam Bangkok 10500</div>
-                    <div>Phone: 0614567777</div>
+                    <div>{form?.Address?.Fullname || ''}</div>
+                    <div>{form?.Address?.Address || ''} {form?.Address?.District || ''} {form?.Address?.Province || ''} {form?.Address?.Zipcode || ''}</div>
+                    <div>Phone: {form?.Address?.Phone || ''}</div>
                     </div>
-                    <div>Tracking Nuber: 111111111111</div>
+                    <div>Tracking Nuber: {form?.Sale_Tracking_Number || '-'}</div>
                 </div>
                 <div className='w-full grid grid-cols-1 md:grid-cols-3 text-[10px] font-light'>
                     <span>PRODUCT</span>
@@ -172,30 +172,33 @@ export const Review = ({ form, onChange, onLoad }) => {
                     <span className='r md:block hidden'>REVIEW</span>
                 </div>
                 <div className='w-full border-y border-brown py-2'>
-                    {[1,1].map((item, index, array) => (
+                    {form?.Product?.map((item, index, array) => (
                         <React.Fragment key={"Customer-Order"+index}>
                             <div className='w-full grid grid-cols-1 md:grid-cols-3'>
                                 <div className='flex gap-2'>
                                     <div>
-                                        <Image src="/assets/images/products/JeanVest.jpg" alt="Product" width={80} height={100}/>
+                                        <Image src={form?.Product?.Product_Image || "/assets/images/avatars/no-image.png"} alt="Product" width={80} height={100}/>
                                     </div>
                                     <div className='flex flex-col font-light'>
                                         <span>Purple Bag</span>
-                                        <span className='text-xs'>Size: S</span>
-                                        <span className='text-xs'>Detail: fggfggfgfg fgfgfgfgfgfg</span>
+                                        <span className='text-xs'>Size: {item?.Size_Name || '-'}</span>
+                                        <span className='text-xs'>Detail: {item?.Product_Size_Detail || '-'}</span>
                                     </div>
                                 </div>
-                                <div className='r font-light'>฿1900.00 Baht</div>
-                                <div className='w-full flex justify-end'><ButtonText onClick={() => onSave()} placeholder='REVIEW' classBox='lg:w-52 sm:w-44 w-full'/></div>
+                                <div className='r font-light'>฿{item?.Product_Price?.toFixed(2) || '-'} Baht</div>
+                                <div className='w-full flex justify-end'><ButtonText onClick={() => onReview()} placeholder='REVIEW' classBox='lg:w-52 sm:w-44 w-full'/></div>
                             </div>  
                             {index !== array.length - 1 && <div className='md:col-start-3 border-b border-gray my-2'></div>}
                         </React.Fragment>
                     ))}
                 </div>
-                <div className='w-full r'>Discount 30% ฿ 624.00 Baht</div>
-                <div className='w-full border-b border-gray'></div>
-                <div className='w-full r'>Subtotal ฿1234.00 Baht</div>
-                <div className='w-full border-b border-gray'></div>
+                {form?.Promotion_Id &&
+                    <>
+                        <div className='w-full r'>{form?.Promotion_Name || ''} ฿{((parseFloat(form?.Sale_Total_Price) * (form?.Promotion_Discount / 100)) || 0).toFixed(2) || '0.00'} Baht</div>
+                        <div className='w-full border-b border-gray'></div> 
+                    </>
+                }
+                <div className='w-full r'>Subtotal ฿{form?.Discounted_Total_Price?.toFixed(2) || form?.Sale_Total_Price?.toFixed(2)} Baht</div>
             </div>
         </div>
     )
