635bc7d4e6b5feb63c6a1db699d7a39846736838
Madcattk <poshnunp@gmail.com>
2023-07-16 13:21:09 +0700

    update member order

--- app/api/getAPI/sale.js
+++ app/api/getAPI/sale.js
@@ -17,3 +17,23 @@ export async function addSaleByMemberId(sale) {
             console.error(error);
         }
 };
+
+export async function getMemberOrderById(Member_Id) {
+    try {
+        const response = await fetch(`http://localhost:3000/api/sale/getMemberOrderById`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({Member_Id}),
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            const errorData = await response.json();
+            console.error(errorData);
+        }
+        } catch (error) {
+            console.error(error);
+        }
+};
--- components/pages/CustomerOrderList.js
+++ components/pages/CustomerOrderList.js
@@ -1,27 +1,64 @@
 "use client";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation';
+import { getMemberOrderById } from '@app/api/getAPI/sale';
+import { getFromLocalStorage, saveToLocalStorage } from '@lib/localStorage';
+import { DateFormat } from '@components/formats';
 
 const CustomerOrderList = () => {
     const router = useRouter();
+    const [auth, setAuth] = useState(null);
+    const [form, setForm] = useState(null);
+    console.log(form);
+
+    useEffect(() => {
+        setAuth(getFromLocalStorage('auth'))
+    },[])
+
+    useEffect(() => {
+        if(auth) onLoad();
+    },[auth])
+    
+    const onLoad = async () => {
+        const res = await getMemberOrderById(auth?.Member_Id)
+        if(res?.message === 'success'){        
+            let address = null;
+            res?.data?.forEach((item, index) => {
+                let add = item?.Delivery_Address.split('%');
+                address = {
+                    Member_Id: item?.Member_Id || '',
+                    Fullname: add[0] || '',
+                    Address: add[1] || '',
+                    District: add[2] || '',
+                    Province: add[3] || '',
+                    Zipcode: add[4] || '',
+                    Phone: add[5] || '',
+                    Member_Address: item?.Delivery_Address || ''
+                }
+            })
+            const updatedData = { ...res.data[0], Address: address };
+            res.data[0] = updatedData;
+            setForm(res?.data || []);
+        }
+    }
     return (
         <>
-            {[1,1,1,1,1].map((item, index) => (
-                <div onClick={() => router.push(`/order/${111}`)} className='cursor-pointer hover:scale-[1.004] transform transition-transform duration-200 w-full flex_center flex-col gap-3 px-5 py-3 border border-brown mb-5' key={"Customer-Order"+index}>
+            {form?.map((item, index) => (
+                <div onClick={() => router.push(`/order/${item?.Sale_Id}`)} className='cursor-pointer hover:scale-[1.004] transform transition-transform duration-200 w-full flex_center flex-col gap-3 px-5 py-3 border border-brown mb-5' key={"Customer-Order"+index}>
                     <div className='w-full flex flex-col md:flex-row md:items-end justify-between font-light'>
-                        <span>ORDER NO: 12342222</span>
-                        <span className='text-sm'>Date: 2023-02-03</span>
-                        <span>Status</span>
+                        <span>ORDER NO: {item?.Sale_Id || ''}</span>
+                        <span className='text-sm'>Date: {DateFormat(item?.Sale_Date) || ''}</span>
+                        <span>{item?.Sale_Status || ''}</span>
                     </div>
                     <div className='w-full py-1 flex flex-col md:flex-row items-start justify-between font-light text-sm border-b border-b-gray border-t border-t-gray'>
                         <div>
-                        <div>Luna Braise</div>
-                        <div>114/1 Siphya Road Mahapruetharam Bangkok 10500</div>
-                        <div>Phone: 0614567777</div>
+                        <div>{item?.Address?.Fullname || ''}</div>
+                        <div>{item?.Address?.Address || ''} {item?.Address?.District || ''} {item?.Address?.Province || ''} {item?.Address?.Zipcode || ''}</div>
+                        <div>Phone: {item?.Address?.Phone || ''}</div>
                         </div>
-                        <div>Tracking Nuber: 111111111111</div>
+                        <div>Tracking Nuber: {item?.Sale_Tracking_Number || '-'}</div>
                     </div>
-                    <div className='w-full r'>Subtotal ฿1234.00 Baht</div>
+                    <div className='w-full r'>Subtotal ฿{item?.Discounted_Total_Price || item?.Sale_Total_Price} Baht</div>
                 </div>
             ))}
         </>
