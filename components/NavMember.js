7f0e8b644484e9bc57ef2b00afb06581bec12a04
Madcattk <poshnunp@gmail.com>
2023-07-26 23:35:48 +0700

    update nav mem

--- components/NavMember.js
+++ components/NavMember.js
@@ -52,7 +52,7 @@ const NavMember = ({ children }) => {
                         <div className="hidden md:flex md:justify-start">
                             <div className="group relative">
                                 <button onClick={() => {setForm({searchInput: '', searchType: '', searchSex: ''});router.push('/');}} className="menu-hover border-none bg-white text-base font-light pr-8">SHOP</button>
-                                <div className={"absolute left-0 pt-2 bg-white z-10 invisible group-hover:visible"}>
+                                <div className={"absolute left-0 pt-2 bg-white z-50 invisible group-hover:visible"}>
                                     {meta?.Product_Type?.map((item, index) => {
                                         return <a key={"Product_Type"+index} onClick={() => {setForm({searchInput: '', searchType: item?.Product_Type_Id, searchSex: ''});router.push('/');}} className="uppercase block pl-1 pr-4 py-2 cursor-pointer hover:bg-hover">{item?.Product_Type_Name}</a>
                                     })}
@@ -60,7 +60,7 @@ const NavMember = ({ children }) => {
                             </div>
                             <div className="group relative">
                                 <button className="menu-hover border-none bg-white text-base font-light pr-8">FILTER</button>
-                                <div className="absolute left-0 pt-2 bg-white z-10 invisible group-hover:visible">
+                                <div className="absolute left-0 pt-2 bg-white z-50 invisible group-hover:visible">
                                 {MetaProductSex?.map((item, index) => {
                                         return <a key={"Product_Type"+index} onClick={() => {setForm({searchInput: '', searchType: '', searchSex: item?.id});router.push('/');}} className="uppercase block pl-1 pr-4 py-2 cursor-pointer hover:bg-hover">{item?.name}</a>
                                     })}
@@ -69,7 +69,7 @@ const NavMember = ({ children }) => {
                         </div>
                         <div className="md:hidden l group relative">
                             <button className="menu-hover"><FontAwesomeIcon icon={faBars} /></button>
-                            <div className="absolute left-0 pt-2 bg-white z-10 invisible group-hover:visible">
+                            <div className="absolute left-0 pt-2 bg-white z-50 invisible group-hover:visible">
                                 <a onClick={() => {setSearch(!search); if(!search)setForm({...form, searchInput: ''});router.push('/');}} className="block px-1 py-2 cursor-pointer hover:bg-hover"><FontAwesomeIcon icon={faSearch} /></a>
                                 <a onClick={() => router.push(`/member/cart`)} className="block px-1 py-2 cursor-pointer hover:bg-hover"><FontAwesomeIcon icon={faShoppingCart} /></a>
                                 <a onClick={() => router.push('/login')} className="block px-1 py-2 cursor-pointer hover:bg-hover"><FontAwesomeIcon icon={faUser} /></a>
