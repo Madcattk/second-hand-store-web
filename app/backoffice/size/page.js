7c9e9d465a7e712ea4478eec6e3ff12a3b21aece
Madcattk <poshnunp@gmail.com>
2023-07-28 12:40:53 +0700

    update show size

--- app/backoffice/size/page.js
+++ app/backoffice/size/page.js
@@ -1,49 +1,48 @@
 "use client"
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Space, Table, Button, Row } from 'antd';
+import { getAllSizes } from '@app/api/getAPI/size';
 const { Column } = Table;
 
+const App = () => {
+    const [data, setData] = useState([]);
 
-const data = [
-    {
-        key: '1',
-        sizeId: '1',
-        sizeName: 'S',
-    },
-    {
-        key: '2',
-        sizeId: '2',
-        sizeName: 'M',
-    },
+    useEffect(() => {
+        const fetchData = async () => {
+            const sizesData = await getAllSizes();
+            setData(sizesData?.data || []);
+        };
+    
+        fetchData();
+    }, []);
 
-];
-
-const App = () => (
-    <>
+    return (
+        <>
         <Row justify="end">
             <Space wrap>
-                <Button type="primary" danger>
-                    Add Size
-                </Button>
+            <Button type="primary" danger>
+                Add Size
+            </Button>
             </Space>
         </Row>
 
-        <Table dataSource={data}>
-            <Column title="Size_Id" dataIndex="sizeId" key="sizeId" />
-            <Column title="Size_Name" dataIndex="sizeName" key="sizeName" />
+        <Table dataSource={data} rowKey="Size_Id">
+            <Column title="Size_Id" dataIndex="Size_Id" key="Size_Id" />
+            <Column title="Size_Name" dataIndex="Size_Name" key="Size_Name" />
             <Column
-                title="Action"
-                key="action"
-                render={(_, record) => (
-                    <Space size="middle">
-                        {/* <a>Invite {record.lastName}</a> */}
-                        <a>Edit</a>
-                        <a>Delete</a>
-                    </Space>
-                )}
+            title="Action"
+            key="action"
+            render={(_, record) => (
+                <Space size="middle">
+                {/* <a>Invite {record.lastName}</a> */}
+                <a>Edit</a>
+                <a>Delete</a>
+                </Space>
+            )}
             />
-
         </Table>
-    </>
-);
+        </>
+    );
+};
+
 export default App;
