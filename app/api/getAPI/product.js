c739e3d8aba991ca0ac448c8999703c4e5fe8eae
Nanpanadda <panaddanakchukaew04@gmail.com>
2023-07-30 18:52:57 +0700

    Product type

--- app/api/getAPI/product.js
+++ app/api/getAPI/product.js
@@ -96,3 +96,43 @@ export async function getProductsBySearch(product) {
             console.error(error);
         }
 };
+
+export async function addProduct(product_name) {
+    try {
+        const response = await fetch(`http://localhost:3000/api/product_name/addProduct`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(product),
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            const errorData = await response.json();
+            console.error(errorData);
+        }
+        } catch (error) {
+            console.error(error);
+        }
+};
+
+export async function editProductById(product_name) {
+    try {
+        const response = await fetch(`http://localhost:3000/api/product/editProductById`, {
+            method: 'PUT',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(product),
+        });
+        if (response.ok) {
+            return await response.json();
+        } else {
+            const errorData = await response.json();
+            console.error(errorData);
+        }
+        } catch (error) {
+            console.error(error);
+        }
+};
