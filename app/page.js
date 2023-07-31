c4ad0a8a81148db90856ffc093f22e2ee36d0a2e
Madcattk <poshnunp@gmail.com>
2023-07-31 13:06:59 +0700

    up y

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div className='flex justify-center w-full'>
         <div className='xl:w-[1200px] lg:w-[900px] md:w-[700px] sm:w-[500px] w-[500px] grid lg:grid-cols-9 md:grid-cols-6 grid-cols-4 gap-4 px-10'>
           <ShopAll/>
+          <div>y</div>
         </div>
     </div>
   )
