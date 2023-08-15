c77dd0df52bbbe3fb8616302b2cdeec82174b2f7
Madcattk <poshnunp@gmail.com>
2023-08-15 21:22:44 +0700

    update loading

--- components/pages/Loading.js
+++ components/pages/Loading.js
@@ -7,7 +7,9 @@ const Loading = ({ loading, children }) => {
             {!loading ?
                 <React.Fragment>{children}</React.Fragment>
             :
-                <></>
+                <div className="absolute right-1/2 bottom-2/3 transform translate-x-1/2 translate-y-1/2 ">
+                    <div className="border-t-transparent border-solid animate-spin rounded-full border-black border-8 h-64 w-64"></div>
+                </div>
             }
         </React.Fragment>
     )
