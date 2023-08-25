a1a4274bff097bc77b095c11c1cf2d6c4ac50c53
Nanpanadda <panaddanakchukaew04@gmail.com>
2023-08-25 19:34:49 +0700

    up account

--- app/backoffice/account/page.js
+++ app/backoffice/account/page.js
@@ -61,62 +61,57 @@ const App = () => {
 
     return (
         <div>
-            <div className='flex justify-between'>
-                <div className='flex gap-3'>
+            <div className='flex justify-between '>
+                <div className='flex gap-5'>
                     <Image src={data?.Employee_Image || "/assets/images/avatars/avartar.jpeg"}
                         alt="Profile image"
                         width={150} height={190}
                         className='object-cover w-[110px] h-[150px]' />
                     <div className='flex flex-col col-span-2'>
-                        <div>ID {data?.Employee_Id} </div>
-                            <div>Firstname {data?.Employee_Firstname}</div>
-                            <div>Lastname {data?.Employee_Lastname}</div>
-                            <div>Sex {data?.Employee_Sex}</div>
-                            <div>Phone {data?.Employee_Phone}</div>
-                            <div>Email {data?.Employee_Email}</div>
-                            <div>Birth Date {DateFormat(data?.Employee_Birth_Date)}</div>
+                        <div>ID: {data?.Employee_Id} </div>
+                        <div>Firstname: {data?.Employee_Firstname}</div> 
+                        <div>Lastname: {data?.Employee_Lastname}</div>
+                        <div>Sex: {data?.Employee_Sex}</div>
+                        <div>Phone: {data?.Employee_Phone}</div>
+                        <div>Email: {data?.Employee_Email}</div>
+                        <div>Birth Date: {DateFormat(data?.Employee_Birth_Date)}</div>
                     </div>
                 </div>
-                <button className='p-2 bd-highlight float-right text-lg font-bold' onClick={() => router.push(`/backoffice/employee/${data.Employee_Id}`)} danger>Edit Profile</button>
+                <button className='flex gap-5' onClick={() => router.push(`/backoffice/employee/${data.Employee_Id}`)} danger>Edit Profile</button>
             </div>
             <div className='w-full grid grid-cols-8 my-3 shadow bg-white text-brown font-light border border-brown'></div>
 
-            <div className='w-full h-fit '>
-
-                <div className='flex'>
-
-                    <div className='flex flex-col gap-3'>
-                        <div className='flex gap-2'>
-                            <textarea
-                                className='border'
-                                name=""
-                                id=""
-                                cols="40"
-                                rows="6"
-                                value={addAddress || ""}
-                                onChange={(e) => setAddAddress(e.target.value)}
-                            ></textarea>
-                            <button onClick={() => onAddAddress()}>Add New Address</button>
-                        </div>
-                        {data?.Addresses?.map((item, index) => {
-                            return <div className='flex gap-2' key={"Employee-Address" + index}>
-                                <textarea
-                                    className='border'
-                                    name=""
-                                    id=""
-                                    cols="40"
-                                    rows="6"
-                                    value={item?.New_Employee_Address || ""}
-                                    onChange={(e) => onChangeAddress(index, e.target.value)}
-                                ></textarea>
-                                <button onClick={() => onEditAddress(index)}>Edit Address</button>
-                            </div>
-
-                        })}
-                    </div>
+            <div className='font-extralight text-3xl pb-5 '>Address</div>
+            <div className='flex flex-col gap-3'>
+                <div className='flex gap-2'>
+                    <textarea
+                        className='border'
+                        name=""
+                        id=""
+                        cols="40"
+                        rows="6"
+                        value={addAddress || ""}
+                        onChange={(e) => setAddAddress(e.target.value)}
+                    ></textarea>
+                    <button onClick={() => onAddAddress()}>Add New Address</button>
                 </div>
+                {data?.Addresses?.map((item, index) => {
+                    return <div className='flex gap-2' key={"Employee-Address" + index}>
+                        <textarea
+                            className='border'
+                            name=""
+                            id=""
+                            cols="40"
+                            rows="6"
+                            value={item?.New_Employee_Address || ""}
+                            onChange={(e) => onChangeAddress(index, e.target.value)}
+                        ></textarea>
+                        <button onClick={() => onEditAddress(index)}>Edit Address</button>
+                    </div>
+                })}
             </div>
         </div>
+
 
     )
 };
