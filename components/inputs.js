9e7a5021402c729d58b3b67a806ef396119e1f72
Madcattk <poshnunp@gmail.com>
2023-08-27 18:43:43 +0700

    update button

--- components/inputs.js
+++ components/inputs.js
@@ -5,13 +5,13 @@ import Image from 'next/image'
 
 export const ButtonText = ({ classBox = "", placeholder = "", onClick }) => (
     <div className={`${classBox}`}>
-        <button onClick={onClick} type={'submit'} className="w-full py-3 border border-brown outline-none c bg-brown hover:bg-white text-white hover:text-brown hover:font-light font-extralight tracking-widest uppercase">{placeholder}</button>
+        <button onClick={onClick} type={'submit'} className="w-full transition-all duration-200 py-3 border border-brown outline-none c bg-brown hover:bg-white text-white hover:text-brown hover:font-light font-extralight tracking-widest uppercase">{placeholder}</button>
     </div>
 );
 
 export const TransparentButtonText = ({ classBox = "", placeholder = "", onClick }) => (
     <div className={`${classBox}`}>
-        <button onClick={onClick} type={'submit'} className="w-full py-3 border border-brown outline-none c bg-white hover:bg-brown text-brown hover:text-white hover:font-extralight font-light tracking-widest uppercase">{placeholder}</button>
+        <button onClick={onClick} type={'submit'} className="w-full transition-all duration-200 py-3 border border-brown outline-none c bg-white hover:bg-brown text-brown hover:text-white hover:font-extralight font-light tracking-widest uppercase">{placeholder}</button>
     </div>
 );
 
@@ -100,7 +100,7 @@ export const InputFile = ({ value = "", onChange, classBox = "", placeholder = "
                 {/* <div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="150" height="180" fill="currentColor" class="bi bi-upload" viewBox="0 0 16 16" id="IconChangeColor"> <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" id="mainIconPathAttribute"></path> <path d="M7.646 1.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 2.707V11.5a.5.5 0 0 1-1 0V2.707L5.354 4.854a.5.5 0 1 1-.708-.708l3-3z" id="mainIconPathAttribute"></path> </svg>
                 </div> */}
-                <button className='w-[150px] py-2 border border-brown outline-none c bg-brown hover:bg-white text-white hover:text-brown hover:font-light font-extralight tracking-widest uppercase' onClick={handleButtonClick}>{buttonText}</button>
+                <button className='w-[150px] transition-all duration-200 py-2 border border-brown outline-none c bg-brown hover:bg-white text-white hover:text-brown hover:font-light font-extralight tracking-widest uppercase' onClick={handleButtonClick}>{buttonText}</button>
                 <input
                     type="file"
                     ref={fileInputRef}
