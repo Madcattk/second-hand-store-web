0860874754b830da02acea4ce5004cbe5a5342d5
Madcattk <poshnunp@gmail.com>
2023-09-10 00:22:10 +0700

    edit pdf (npm install @react-pdf/renderer --save)

--- app/backoffice/pdf/[id]/page.js
+++ app/backoffice/pdf/[id]/page.js
@@ -2,10 +2,8 @@
 import React, { useEffect, useState } from 'react';
 import { getBestSellerProductReport } from '@app/api/getAPI/sale';
 import { DateFormat } from '@components/formats';
-import html2pdf from 'html2pdf.js';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useParams } from 'next/navigation';
-import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
 
 const page = () => {
     const { id } = useParams()
@@ -16,6 +14,43 @@ const page = () => {
         End_Date: _d?.[1]
     });
     let total = 0;
+    const styles = StyleSheet.create({
+        page: {
+          flexDirection: 'portrait', // Portrait orientation
+          backgroundColor: 'white', // A4 page is typically white
+          width: '210mm', // A4 width in millimeters
+          height: '297mm', // A4 height in millimeters
+          paddingTop: '10mm', // Top margin
+          paddingRight: '10mm', // Right margin
+          paddingBottom: '10mm', // Bottom margin
+          paddingLeft: '10mm', // Left margin
+        },
+        section: {
+            margin: 10,
+            padding: 10,
+            flexGrow: 1,
+        },
+        text: {
+            fontSize: 10,
+            marginBottom: 5,
+        },
+        table: {
+            display: 'table',
+            width: '100%',
+        },
+        tableRowTH: {
+            flexDirection: 'row',
+            backgroundColor: '#eee9',
+            borderBottomColor: '#eee', // Set the border color for horizontal lines
+            borderBottomWidth: 1,   // Set the border width for horizontal lines
+            fontFamily: 'Helvetica-Bold',
+        },
+        tableRow: {
+            flexDirection: 'row',
+            borderBottomColor: '#eee', // Set the border color for horizontal lines
+            borderBottomWidth: 1,   // Set the border width for horizontal lines
+        },
+    });
 
     useEffect(() => {
         onLoad()
@@ -33,16 +68,6 @@ const page = () => {
         }
     }
 
-    const convertToPDF = () => {
-        const content = document.getElementById('pdf-content');
-        // html2pdf().from(content).save();
-        html2pdf().from(content)
-            .set()
-            .toPdf()
-            .get('pdf')
-            .then((pdf) => downloadPdf(pdf));
-    };
-
     const downloadPdf = (pdf) => {
         let link = document.createElement('a');
         link.target = '_blank';
@@ -54,55 +79,93 @@ const page = () => {
 
     return (
         <div className='flex_center w-full h-full gap-10'>
-            <div className='px-16 pb-16 h-fit w-fit bg-gray rounded-lg shadow-md'>
-                <div className='w-full flex justify-end'>
-                    <button className='cursor-pointer py-2 px-4 my-4 shadow-md rounded-lg bg-white hover:bg-hover ' onClick={convertToPDF}>Download <FontAwesomeIcon icon={faFileArrowDown} size="lg" /></button>
-                </div>
-                <div className='border bg-white h-[297mm] overflow-y-auto'>
-                    <div id="pdf-content" className={`w-[210mm] text-[12px] bg-white px-5`}>
-                        <div className='w-full flex justify-center font-bold text-[14px] py-5'>Report for the period: {date?.Start_Date} to {date?.End_Date}</div>
-                        <div className='font-bold py-3 text-[13px]'>Best Seller Product Report</div>
-                        <div className='w-full flex flex-col gap-1'>
-                            {form?.map((item, index) => {
-                                return <div className={`border-b border-hover w-full pb-3 text-brown`} key={"Best-Seller-Product"+index}>
-                                    <div className='pb-1 w-full font-bold'>{item?.Product_Type_Name || ''}</div>
-                                    <div className=''>Total sales: {item?.Count || '0'}</div>
-                                    <div className=''>Total price: <span className='font-bold text-red-500'>฿{item?.Total_Price.toFixed(2) || '0'}</span> Baht</div>
-                                </div>
+            <PDFViewer className='w-screen h-screen'>
+                <Document>
+                    <Page size="A4" style={styles.page}>
+                        <View style={styles.section}>
+                        <View style={{ display: 'flex', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 12, paddingBottom: 10, fontFamily: 'Helvetica-Bold' }}>Report for the period: {date?.Start_Date} to {date?.End_Date}</Text>
+                        </View>
+                        <Text style={{ fontSize: 12, paddingBottom: 10, paddingTop: 10, fontFamily: 'Helvetica-Bold'  }}>Best Seller Product Report</Text>
+                        {form?.map((item, index) => {
+                            return  <View key={'Best-Seller'+index} style={{ marginBottom: 5}}>
+                            <Text style={{ fontSize: 11, paddingBottom: 5, fontFamily: 'Helvetica-Bold' }}>{item?.Product_Type_Name || ''}</Text>
+                            <Text style={{fontSize: 11}}>Sales: {item?.Count || '0'}</Text>
+                            <View style={{ flexDirection: 'row', paddingBottom: 5, borderBottom: 1, borderBottomColor: '#eee' }}>
+                                <Text style={{ fontSize: 11, color: 'black' }}>Total:</Text>
+                                <Text style={{ fontSize: 11, color: '#D71313', fontFamily: 'Helvetica-Bold' }}> {item?.Total_Price.toFixed(2) || '0'} </Text>
+                                <Text style={{ fontSize: 11, color: 'black' }}>Baht</Text>
+                            </View>
+                            </View>
+                        })}
+                        </View>
+                    </Page>
+                    <Page size="A4" style={styles.page}>
+                        <View style={styles.section}>
+                        <Text style={{ fontSize: 12, paddingBottom: 10, paddingTop: 10, fontFamily: 'Helvetica-Bold'  }}>Summary Revenue</Text>
+                        {/* Table */}
+                        <View style={styles.table}>
+                            <View style={styles.tableRowTH}>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'left',}}>
+                                    <Text style={{ fontSize: 11 }}>Product Name</Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'left',}}>
+                                    <Text style={{ fontSize: 11 }}>Product Type Name</Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'center',}}>
+                                    <Text style={{ fontSize: 11 }}>Sale Date</Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'center',}}>
+                                    <Text style={{ fontSize: 11 }}>Sale Status</Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'right',}}>
+                                    <Text style={{ fontSize: 11 }}>Product Price</Text>
+                                </View>
+                            </View>
+                            {form?.map((item) => {
+                                return item?.Product?.map((product, index) => {
+                                    total += product?.Product_Price;
+                                return <View key={'summary'+index} style={styles.tableRow}>
+                                    <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'left',}}>
+                                        <Text style={{ fontSize: 11 }}>{product?.Product_Name || '-'}</Text>
+                                    </View>
+                                    <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'left',}}>
+                                        <Text style={{ fontSize: 11 }}>{product?.Product_Type_Name || '-'}</Text>
+                                    </View>
+                                    <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'center',}}>
+                                        <Text style={{ fontSize: 11 }}>{DateFormat(product?.Sale_Date) || '-'}</Text>
+                                    </View>
+                                    <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'center',}}>
+                                        <Text style={{ fontSize: 11 }}>{product?.Sale_Status || '-'}</Text>
+                                    </View>
+                                    <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'right',}}>
+                                        <Text style={{ fontSize: 11 }}>{product?.Product_Price.toFixed(2) || '0.00'}</Text>
+                                    </View>
+                                </View>
+                                })
                             })}
-                        </div>
-                        <div className='font-bold py-4 text-[13px]'>Summary Revenue</div>
-                        <table className='table text-brown'>
-                            <thead>
-                                <tr className='border-y border-hover bg-hover'>
-                                    <th className='w-2/12 l p-2'>Product Name</th>
-                                    <th className='w-2/12 l p-2'>Product Type Name</th>
-                                    <th className='w-2/12 c p-2'>Sale Date</th>
-                                    <th className='w-2/12 c p-2'>Sale Status</th>
-                                    <th className='w-2/12 r p-2'>Product Price</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {form?.map((item) => {
-                                    return item?.Product?.map((product, index) => {
-                                        total += product?.Product_Price;
-                                        return <tr className='items-center border-b border-hover' key={"Summary-Revenue"+index}>
-                                            <td className='l p-2'>{product?.Product_Name || '-'}</td>
-                                            <td className='l p-2'>{product?.Product_Type_Name || '-'}</td>
-                                            <td className='c p-2'>{DateFormat(product?.Sale_Date) || '-'}</td>
-                                            <td className='c p-2'>{product?.Sale_Status || '-'}</td>
-                                            <td className='r p-2'>฿{product?.Product_Price.toFixed(2) || '0.00'}</td>
-                                        </tr>
-                                    })
-                                })}
-                            </tbody>
-                        </table>
-                        <div className='w-full bg-hover text-brown flex items-center justify-end py-2 mb-5 r font-bold'>
-                            Total Revenue: ฿{total.toFixed(2) || '0.00'} Baht
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            <View style={styles.tableRowTH}>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'left',}}>
+                                    <Text style={{ fontSize: 11 }}></Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'left',}}>
+                                    <Text style={{ fontSize: 11 }}></Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'center',}}>
+                                    <Text style={{ fontSize: 11 }}></Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'right',}}>
+                                    <Text style={{ fontSize: 11 }}>Total Revenue:</Text>
+                                </View>
+                                <View style={{ borderWidth: 0, padding: 7, flex: 1, textAlign: 'right',}}>
+                                    <Text style={{ fontSize: 11 }}>{total.toFixed(2) || '0.00'} Baht</Text>
+                                </View>
+                            </View>
+                        </View>
+                        </View>
+                    </Page>
+                </Document>
+            </PDFViewer>
         </div>
     )
 }
