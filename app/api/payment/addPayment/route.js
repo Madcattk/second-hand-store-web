7b20f5dbef6397ccb89190037b0bd4f051606465
Madcattk <poshnunp@gmail.com>
2023-09-11 12:17:44 +0700

    up date in payment table

--- app/api/payment/addPayment/route.js
+++ app/api/payment/addPayment/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import dbConnection from "@/lib/db";
-import { DateTimeFormat } from "@components/formats";
+import { DateFormat } from "@components/formats";
 
 export async function POST(request) {
     const { Payment_Slip, Sale_Id } = await request.json();
@@ -10,7 +10,7 @@ export async function POST(request) {
         `INSERT INTO Payment 
         (Payment_Slip, Sale_Id, Payment_Date) 
         VALUES (?, ?, ?)`;
-        const values = [Payment_Slip, Sale_Id, DateTimeFormat(new Date())];
+        const values = [Payment_Slip, Sale_Id, DateFormat(new Date())];
         const [result] = await conn.execute(query, values);
         conn.end();
         return NextResponse.json({ data: result, message: "success" }, { status: 201 });
