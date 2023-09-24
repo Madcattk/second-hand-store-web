9a7e642e3fcf96c52aba90f80db49ca85d33ea24
Madcattk <poshnunp@gmail.com>
2023-09-24 16:55:31 +0700

    up back search product

--- app/backoffice/product/page.js
+++ app/backoffice/product/page.js
@@ -1,15 +1,160 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import { Space, Table, Button, Row } from 'antd';
-import { useRouter } from 'next/navigation';
-import { getAllProducts, deleteProductById } from '@app/api/getAPI/product';
+import { getAllProducts, deleteProductById, getProductsBySearch } from '@app/api/getAPI/product';
 import { DateFormat } from '@components/formats';
 import { Image } from 'antd'
 import Swal from 'sweetalert2';
-import { MetaProductStatus } from '@components/Meta';
-
+import { MetaProductSex, MetaProductStatus } from '@components/Meta';
+import { Col, Form, Input, Select, theme } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import { getAllProductTypes } from '@app/api/getAPI/product-type';
+import { useRouter } from 'next/navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsRotate } from '@fortawesome/free-solid-svg-icons';
+const { Option } = Select;
 const { Column } = Table;
+
+const AdvancedSearchForm = ({data, setData, router, expand, setExpand, onLoadOldData}) => {
+    const { token } = theme.useToken();
+    const [form] = Form.useForm();
+    const [meta, setMeta] = useState({})
+    const formStyle = {
+        maxWidth: 'none',
+        borderRadius: token.borderRadiusLG,
+        padding: 24,
+    };
+    useEffect(() => {
+        onLoad()
+    },[])
+
+    const onLoad = async() => {
+        const resProductType = await getAllProductTypes();
+        const productTypes = resProductType?.data?.map((item, index) => {
+            return {
+                id: item?.Product_Type_Id,
+                name: item?.Product_Type_Name
+            }
+        })
+        setMeta({
+            Product_Types: productTypes
+        })
+    }
+
+    const getFields = () => {
+        const count = expand ? 3 : 0;
+        const children = [];
+        for (let i = 0; i < count; i++) {
+        children.push(
+            <Col span={8} key={i}>
+            {i % 3 !== 2 ? (
+                <Form.Item
+                    name={`${i === 0 ? 'Product_Type' : 'Product_Sex'}`}
+                    label={`${i === 0 ? 'Product Type' : 'Product Sex'}`}
+                >
+                    <Select 
+                        onChange={() => onChange(`${i === 0 ? 'Product_Type' : 'Product_Sex'}`)}
+                    >
+                        {i === 0 ?
+                            meta?.Product_Types?.map((item, index) => {
+                                return <Select.Option key={"Product-Type" + index} value={item.id}>{item.name}</Select.Option>
+                            })
+                        :
+                            MetaProductSex.map((item, index) => {
+                                return <Select.Option key={"Sex" + index} value={item.id}>{item.name}</Select.Option>
+                            })
+                        }
+                    </Select>
+                </Form.Item>
+            ) : (
+                <Form.Item
+                    name={`Search_Input`}
+                    label={`Search Input`}
+                    onChange={() => onChange('Search_Input')}
+                >
+                    <Input placeholder="placeholder" />
+                </Form.Item>
+            )}
+            </Col>,
+        );
+        }
+        return children;
+    };
+
+    const onChange = (fieldName) => {
+        if(fieldName === "Product_Type") {
+            form.resetFields(["Product_Sex", "Search_Input"])
+        } else if (fieldName === "Product_Sex") {
+            form.resetFields(["Product_Type", "Search_Input"])
+        }else if (fieldName === "Search_Input") {
+            form.resetFields(["Product_Type", "Product_Sex"])
+        }
+    };
+
+    const onFinish = async(values) => {
+        if(values?.Search_Input === undefined && values?.Product_Type === undefined && values?.Product_Sex === undefined) {
+            onLoadOldData()
+            return
+        }
+        const res = await getProductsBySearch({
+            'searchInput': values?.Search_Input,
+            'searchType': values?.Product_Type,
+            'searchSex': values?.Product_Sex,
+        });
+        if(res?.message === 'success' && res?.data){
+            let data = [];
+            res?.data?.forEach((item, index) => {
+                data.push({ ...item, Product_Date: DateFormat(item.Product_Date), Product_Price: item.Product_Price.toFixed(2) })
+            })
+            setData(data || []);
+        }
+    };
+    return (
+        <Form className='sticky top-0 z-50 bg-white' form={form} name="advanced_search" style={formStyle} onFinish={onFinish}>
+            <Row gutter={24}>{getFields()}</Row>
+            <Row justify="space-between">
+                <Space wrap>
+                    <div className='font-semibold py-2'>Product amount: {data?.length || '-'}</div>
+                    <FontAwesomeIcon icon={faArrowsRotate} className='cursor-pointer hover:bg-hover p-2 rounded' onClick={() => {onLoadOldData(); form.resetFields();}}/>
+                </Space>
+
+                <Space wrap>
+                    <Button onClick={() => router.push('/backoffice/product/addproduct')} type="primary" danger htmlType='button'>
+                            Add Product
+                    </Button>
+                    {expand &&
+                    <>
+                        <Button type="primary" danger htmlType="submit">
+                            Search
+                        </Button>
+                        <Button
+                            onClick={() => {
+                            form.resetFields();
+                            }}
+                        >
+                            Clear
+                        </Button>
+                    </>
+                    }
+                    <a
+                        style={{
+                        fontSize: 12,
+                        }}
+                        onClick={() => {
+                        setExpand(!expand);
+                        }}
+                    >
+                        <DownOutlined rotate={expand ? 180 : 0} /> Collapse
+                    </a>
+                </Space>
+            </Row>
+        </Form>
+    );
+};
+
 const App = () => {
+    const { token } = theme.useToken();
+    const [expand, setExpand] = useState(true);
     const router = useRouter();
     const [data, setData] = useState([]);
     useEffect(() => {
@@ -52,19 +197,9 @@ const App = () => {
     };
 
     return (
-        <>
-            <div className='w-full sticky top-0 z-50 h-16 py-1 px-3 bg-white flex justify-between items-center'>
-                <Space wrap>
-                    <div className='font-semibold'>Product amount: {data?.length || '-'}</div>
-                </Space>
-                <Space wrap>
-                    <Button onClick={() => router.push('/backoffice/product/addproduct')} type="primary" danger>
-                        Add Product
-                    </Button>
-                </Space>
-            </div>
-
-            <Table dataSource={data} scroll={{x: 1500}} rowKey="Product_Id" sticky={{offsetHeader: 64,}}>
+        <div className='w-full ralative'>
+            <AdvancedSearchForm data={data} setData={setData} router={router} expand={expand} setExpand={setExpand} onLoadOldData={onLoad}/>
+            <Table dataSource={data} scroll={{x: 1500}} rowKey="Product_Id" sticky={{offsetHeader: expand ? 140 : 85,}} >
                 <Column title="ID" dataIndex="Product_Id" key="Product_Id" fixed='left' width={60} />
                 <Column
                     title="Image"
@@ -116,7 +251,7 @@ const App = () => {
                     )}
                 />
             </Table>
-        </>
+        </div>
     );
 }
 
